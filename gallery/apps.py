ac07d410595fe1f8115e809d3b3283ecdb23c8be
dtk <dtk@gmx.de>
2018-09-09 23:36:00 +0200

    Fix `apps` name

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from redis import StrictRedis
+
+
+cache = None
 
 
 class GalleryConfig(AppConfig):
     name = 'gallery'
+    verbose_name = 'Physalis'
+
+    def ready(self):
+        global cache
+        cache = StrictRedis('172.17.0.2')
